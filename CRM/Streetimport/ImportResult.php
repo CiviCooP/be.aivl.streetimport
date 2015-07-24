7494bd988fec7487842b52628fa60d8582fcc979
systopia <endres@systopia.de>
2015-07-24 14:29:29 +0200

    WIP: improved logging

--- CRM/Streetimport/ImportResult.php
+++ CRM/Streetimport/ImportResult.php
@@ -1,14 +1,14 @@
 <?php
 
-define(DEBUG, 0);
-define(INFO,  1);
-define(WARN,  2);
-define(ERROR, 3);
-define(FATAL, 4);
-define(OFF,   10);
+define(BE_AIVL_STREETIMPORT_DEBUG, 0);
+define(BE_AIVL_STREETIMPORT_INFO,  1);
+define(BE_AIVL_STREETIMPORT_WARN,  2);
+define(BE_AIVL_STREETIMPORT_ERROR, 3);
+define(BE_AIVL_STREETIMPORT_FATAL, 4);
+define(BE_AIVL_STREETIMPORT_OFF,   10);
 
-define(LOGGING_THRESHOLD,        DEBUG );
-define(ERROR_CONSOLE_THRESHOLD,  DEBUG );
+define(LOGGING_THRESHOLD,  BE_AIVL_STREETIMPORT_DEBUG );
+define(CONSOLE_THRESHOLD,  BE_AIVL_STREETIMPORT_INFO  );
 
 
 /**
@@ -28,92 +28,127 @@ class CRM_Streetimport_ImportResult {
   // stats
   protected $import_success  = array();
   protected $import_fail     = array();
-  protected $max_error_level = DEBUG;
+  protected $max_error_level = BE_AIVL_STREETIMPORT_DEBUG;
+
+  // config, for convenience
+  protected $config          = NULL;
+
+  // logging thresholds (will be overwritten by config)
+  protected $logging_threshold = BE_AIVL_STREETIMPORT_DEBUG;
+  protected $console_threshold = BE_AIVL_STREETIMPORT_INFO;
+  protected $file_threshold    = BE_AIVL_STREETIMPORT_INFO;
+
+  // simple constructor
+  function __construct($context) {
+    $this->$config = CRM_Streetimport_Config::singleton();
+
+    // TODO: open log file
+
+    // TODO: get/override log thresholds from config
+
+  }
 
   /**
    * log the import of an individual record
    *
-   * @param $id       unique id string for the record (e.g. line number)
+   * @param $record   the whole record
    * @param $success  true if successfully processed 
    * @param $type     optional string representing the type of record
    * @param $message  optional additional message
    */
-  public function logImport($id, $success, $type = '', $message = '') {
-    $config = CRM_Streetimport_Config::singleton();
+  public function logImport($record, $success, $type = '', $message = '') {
+    $id = $this->getIDforRecord($record);
     if ($success) {
-      $this->logMessage($config->translate("Successfully imported record")." ".$id." :".$message.", ".DEBUG.", ".$type);
       if (!isset($this->import_success[$id])) $this->import_success[$id] = NULL;
+      $this->logMessage(
+        $this->config->translate("Successfully imported record") . " [$type]: $message",
+        $record,
+        BE_AIVL_STREETIMPORT_DEBUG);
     } else {
-      $this->logMessage($config->translate("Failed to import record")." ".$id." :".$message.", ".WARN.", ".$type);
       if (!isset($this->import_fail[$id])) $this->import_fail[$id] = NULL;
       if (isset($this->import_success[$id])) unset($this->import_success[$id]);
+      $this->logMessage(
+        $this->config->translate("Failed to import record") . " [$type]: $message",
+        $record,
+        BE_AIVL_STREETIMPORT_WARN);
     }
   }
 
   /**
    * Log a message or error
    */
-  public function logMessage($message, $error_level = INFO, $type='') {
-    if ($error_level > LOGGING_THRESHOLD) {
+  public function logMessage($message, $record, $log_level = BE_AIVL_STREETIMPORT_INFO) {
+    if ($log_level > $this->logging_threshold) {
       $this->log_entries[] = array(
         'timestamp' => date('Y-m-d h:i:s'),
-        'log_level' => $error_level,
-        'type'      => $type,
+        'id'        => $this->getIDforRecord($record),
+        'log_level' => $log_level,
         'message'   => $message,
         );      
     }
 
-    if ($error_level > $this->max_error_level) {
-      $this->max_error_level = $error_level;
+    // keep track of the 'worst' message so far
+    if ($log_level > $this->max_error_level) {
+      $this->max_error_level = $log_level;
     }
 
-    if ($error_level >= ERROR_CONSOLE_THRESHOLD) {
-      error_log("$error_level: $message");
+    // log to console
+    if ($log_level >= $this->console_threshold) {
+      error_log("$log_level: $message");
+    }
+
+    // log to file
+    if ($log_level >= $this->file_threshold) {
+      // TODO: log to file
     }
   }
 
   /**
-   * shortcut for logMessage($message, DEBUG)
+   * shortcut for logMessage($message, BE_AIVL_STREETIMPORT_DEBUG)
    */
-  public function logDebug($message) {
-    $this->logMessage($message, DEBUG);
+  public function logDebug($message, $record) {
+    $this->logMessage($message, $record, BE_AIVL_STREETIMPORT_DEBUG);
   }
 
   /**
-   * shortcut for logMessage($message, WARN)
+   * shortcut for logMessage($message, BE_AIVL_STREETIMPORT_WARN)
    */
-  public function logWarning($message) {
-    $this->logMessage($message, WARN);
+  public function logWarning($message, $record) {
+    $this->logMessage($message, $record, BE_AIVL_STREETIMPORT_WARN);
   }
 
   /**
-   * shortcut for logMessage($message, ERROR)
+   * shortcut for logMessage($message, BE_AIVL_STREETIMPORT_ERROR)
+   * Will also create an activity
    */
-  public function logError($message, $source = "Unknown", $line_id = "n/a", $title = "Import Error") {
-    $this->logMessage($message, ERROR);
-    $this->createErrorActivity($message, $source, $line_id, $title);
+  public function logError($message, $record, $title = NULL) {
+    $this->logMessage($message, $record, BE_AIVL_STREETIMPORT_ERROR);
+
+    if ($title==NULL) $title = substr($message, 0, 64);
+    $title = $this->config->translate($title);
+    $this->createErrorActivity($message, $record, $title);
   }
 
   /**
-   * shortcut for logMessage($message, ERROR)
+   * shortcut for logMessage($message, BE_AIVL_STREETIMPORT_ERROR)
+   * Will also create an activity
    * @param abort  if true, an exception will be raised, stopping the execution
    */
-  public function logFatal($message, $source = "Unknown", $line_id = "n/a", $title = "Import Failure") {
-    $config = CRM_Streetimport_Config::singleton();
-    $source = $config->translate($source);
-    $line_id = $config->translate($line_id);
-    $title = $config->translate($title);
-    $this->logMessage($message, FATAL);
-    $this->createErrorActivity($message, $source, $line_id, $title);
+  public function logFatal($message, $record, $title = NULL) {
+    $this->logMessage($message, $record, BE_AIVL_STREETIMPORT_FATAL);
+
+    if ($title==NULL) $title = substr($message, 0, 64);
+    $title = $this->config->translate($title);
+    $this->createErrorActivity($message, $record, $title);
   }
 
   /**
    * shortcut for logFatal AND throwing an exception (with the same message)
    * @throws Exception
    */
-  public function abort($message) {
-    $this->logFatal($message);
-    // TODO: use a specific exception type?
+  public function abort($message, $record) {
+    $this->logFatal($message, $record);
+    // TODO: use a specific exception type? add more Information?
     throw new Exception($message);
   }
 
@@ -143,10 +178,9 @@ class CRM_Streetimport_ImportResult {
    */
   public function toAPIResult() {
     $counts = count($this->import_success) . " of " . (count($this->import_success)+count($this->import_fail)) . " records imported.";
-    if ($this->max_error_level >= FATAL) {
-      $config = CRM_Streetimport_Config::singleton();
-      $fatal_messages = $this->getEntriesWithLevel(FATAL, true);
-      $message = $config->translate("FATAL ERROR(S)").": ". implode(', ', $fatal_messages) . ". $counts";
+    if ($this->max_error_level >= BE_AIVL_STREETIMPORT_FATAL) {
+      $fatal_messages = $this->getEntriesWithLevel(BE_AIVL_STREETIMPORT_FATAL, true);
+      $message = $this->config->translate("FATAL ERROR(S)").": ". implode(', ', $fatal_messages) . ". $counts";
       return civicrm_api3_create_error($message);
     } else {
       return civicrm_api3_create_success($counts);
@@ -157,32 +191,58 @@ class CRM_Streetimport_ImportResult {
    * This will create an "Error" activity assigned to the admin
    * @see https://github.com/CiviCooP/be.aivl.streetimport/issues/11
    */
-  protected function createErrorActivity($message, $source = "Unknown", $line_id = "n/a", $title = "Import Error") {
-    $config = CRM_Streetimport_Config::singleton();
-    try {  // AVOID raising anothe excption leading to this
+  protected function createErrorActivity($message, $record, $title = "Import Error") {
+    try {  // AVOID raising another exception leading to this very handler
 
       // TOOD: replace this ugly workaround:
       $handler = new CRM_Streetimport_StreetRecruitmentRecordHandler($this);
 
       // create the activity
       $activity_info = array(
-        'message' => $config->translate($message),
-        'title'   => $config->translate($title),
-        'source'  => $config->translate($source),
-        'line_id' => $config->translate($line_id));
+        'message' => $this->config->translate($message),
+        'title'   => $this->config->translate($title),
+        'record'  => $record,
+        'id'      => $this->getIDforRecord($record));
       $handler->createActivity(array(
-                            'activity_type_id'   => $config->getImportErrorActivityType(),
-                            'subject'            => $config->translate($title),
-                            'status_id'          => $config->getImportErrorActivityStatusId(),
+                            'activity_type_id'   => $this->config->getImportErrorActivityType(),
+                            'subject'            => $this->config->translate($title),
+                            'status_id'          => $this->config->getImportErrorActivityStatusId(),
                             'activity_date_time' => date('YmdHis'),
-                            // 'target_contact_id'  => (int) $config->getAdminContactID(),
-                            'source_contact_id'  => (int) $config->getAdminContactID(),
-                            'assignee_contact_id'=> (int) $config->getAdminContactID(),
+                            // 'target_contact_id'  => (int) $this->config->getAdminContactID(),
+                            'source_contact_id'  => (int) $this->config->getAdminContactID(),
+                            'assignee_contact_id'=> (int) $this->config->getAdminContactID(),
                             'details'            => $handler->renderTemplate('activities/ImportError.tpl', $activity_info),
                             ));
       
     } catch (Exception $e) {
-      error_log($config->translate("Error while creating an activity to report another error").": " . $e->getMessage());
+      error_log($this->config->translate("Error while creating an activity to report another error").": " . $e->getMessage());
     }
   }
+
+  /**
+   * generate a descriptive ID for the given record
+   */
+  public function getIDforRecord(&$record) {
+    return $record['source'] . ':' . $record['id'];
+  }
+
+  /**
+   * Resolve log level
+   */
+  public function resolveLogLevel($level) {
+    switch ($level) {
+      case BE_AIVL_STREETIMPORT_DEBUG:
+        return $this->config->translate('DEBUG');
+      case BE_AIVL_STREETIMPORT_INFO:
+        return $this->config->translate('INFO');
+      case BE_AIVL_STREETIMPORT_WARN:
+        return $this->config->translate('WARN');
+      case BE_AIVL_STREETIMPORT_ERROR:
+        return $this->config->translate('ERROR');
+      case BE_AIVL_STREETIMPORT_FATAL:
+        return $this->config->translate('FATAL');
+      default:
+        return $this->config->translate('UNKNOWN');
+    }
+  } 
 }
