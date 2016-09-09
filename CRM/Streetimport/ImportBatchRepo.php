47c6526187ab6dccbd85fe524bb43fa4a90f76a6
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2016-09-09 19:39:09 +0100

    WIP #625

--- CRM/Streetimport/ImportBatchRepo.php
+++ CRM/Streetimport/ImportBatchRepo.php
@@ -5,10 +5,7 @@ class CRM_Streetimport_ImportBatchRepo{
     function getAllProcessedImports(){
         $config = CRM_Streetimport_Config::singleton();
         foreach(glob($config->getProcessedFileLocation().'*.csv') as $import){
-            $imports[]=array(
-                'id'=>pathinfo($import)['filename'],
-                'path'=>$import
-            );
+            $imports[]=new CRM_Streetimport_ImportBatch(pathinfo($import)['filename']);
         }
         return $imports;
     }
