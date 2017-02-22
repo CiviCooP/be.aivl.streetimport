058ad33802cdf7173b2fd0632ef3de61d731c536
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-02-22 17:50:13 +0100

    don't use saveBankAccount method

--- CRM/Streetimport/Form/CreateMandate.php
+++ CRM/Streetimport/Form/CreateMandate.php
@@ -46,7 +46,6 @@ class CRM_Streetimport_Form_CreateMandate extends CRM_Core_Form
         $fieldNames['bank_name'] = 'sdd_bank_name';
         $this->add('text', 'bank_name', ts('Bank name'));
 
-
         // create a record for the extractor to process
         $activity = civicrm_api3('activity', 'getsingle', array('id' => $this->get('activity_id')));
         foreach ($fieldNames as $recordKey => $customFieldName) {
@@ -68,13 +67,14 @@ class CRM_Streetimport_Form_CreateMandate extends CRM_Core_Form
     public function postProcess()
     {
         // collect valid params submitted info, form vars, and sensible defaults
+        //
         $validKeys = array(
           'amount' => null,
           'reference' => null,
           'frequency_interval' => null,
           'iban' => null,
           'bic' => null,
-          'bank_name' => null
+          'bank_name' => null,
         );
 
         $params = array_intersect_key($this->getSubmitValues(), $validKeys);
@@ -82,54 +82,78 @@ class CRM_Streetimport_Form_CreateMandate extends CRM_Core_Form
         $params['type'] = 'RCUR'; // Should always be recur
         $params['contact_id'] = $this->get('contact_id');
 
-        $mandate_data = array(
-          'contact_id' => $params['contact_id'],
-          'iban' => $params['iban'],
-          'bic' => $params['bic'],
-          'bank_name' => $params['bank_name']
-        );
+        // create a bank account (if necessary)
 
-        // use the already existing saveBankAccount method to save bank account details
+        $account_exists = false;
+        $type_id_IBAN = (int) CRM_Core_OptionGroup::getValue('civicrm_banking.reference_types', 'IBAN', 'name', 'String', 'id');
 
-        // create a simple logger that will be useful if we need to display any of
-        // the logged messages that ::saveBankAccountgenerates to the user, and also
-        // keeps tracks of whether any errors have occured.
+        try {
 
-        $logger = new onScreenLogger();
-        $rh = new CRM_Streetimport_StreetRecruitmentRecordHandler($logger);
-        $record = '';
-        $rh->saveBankAccount($mandate_data, $record);
-        try{
-          $result = civicrm_api3('SepaMandate', 'createfull', $params);
-        }catch(Exception $e){
-          CRM_Core_Session::setStatus($e->getMessage(), 'Could not create mandate', 'alert');
-          $result = array('is_error' => 1);
+          // check the user's bank accounts
+          $ba_list = civicrm_api3('BankingAccount', 'get', array('contact_id' => $params['contact_id']));
+            foreach ($ba_list['values'] as $ba_id => $ba) {
+                $ref_query = array('ba_id' => $ba['id'], 'reference_type_id' => $type_id_IBAN);
+                $ba_ref_list = civicrm_api3('BankingAccountReference', 'get', $ref_query);
+                foreach ($ba_ref_list['values'] as $ba_ref_id => $ba_ref) {
+                    if ($ba_ref['reference'] == $params['iban']) {
+                        $account_exists = true;
+                        break 2;
+                    }
+                }
+            }
+
+            if (!$account_exists) {
+                $config = CRM_Streetimport_Config::singleton();
+            // create bank account (using BAOs)
+            $baExtraSource = $config->translate('Street Recruitment');
+                $baDescription = $config->translate('Private Account');
+                $ba_extra = array(
+                    'BIC' => $params['bic'],
+                    'country' => substr($params['iban'], 0, 2),
+                    'source' => $baExtraSource,
+            );
+                if (!empty($params['bank_name'])) {
+                    $ba_extra['bank_name'] = $params['bank_name'];
+                }
+
+                $ba = civicrm_api3('BankingAccount', 'create', array(
+                    'contact_id' => $params['contact_id'],
+                    'description' => $baDescription,
+                    'created_date' => date('YmdHis'),
+                    'data_raw' => '{}',
+                    'data_parsed' => json_encode($ba_extra),
+                ));
+
+            // add a reference
+            civicrm_api3('BankingAccountReference', 'create', array(
+                'reference' => $params['iban'],
+                'reference_type_id' => $type_id_IBAN,
+                'ba_id' => $ba['id'],
+            ));
+            }
+            CRM_Core_Session::setStatus('Bank account saved', 'info');
+        } catch (Exception $ex) {
+            CRM_Core_Session::setStatus('An error occurred while saving the bank account', 'info');
+            $result = array('is_error' => 1);
         }
-        if(!$result['is_error'] && !$logger->error){
-            CRM_Core_Session::setStatus('All is well', 'Mandate created', 'info');
-            CRM_Utils_System::redirect('/civicrm/activity?atype='.$this->get('activity_type_id').'&action=view&reset=1&id='.$this->get('activity_id').'&cid='.$this->get('contact_id').'&context=activity&searchContext=activity');
-        } else {
+
+        // create a mandate
+
+        try {
+            $result = civicrm_api3('SepaMandate', 'createfull', $params);
+        } catch (Exception $e) {
+            CRM_Core_Session::setStatus($e->getMessage(), 'Could not create mandate', 'alert');
+            $result = array('is_error' => 1);
         }
+
+        // If there are no errors, report success
+
+        if (!$result['is_error']) {
+            CRM_Core_Session::setStatus('Mandate created', 'info');
+        }
+
+        // redirect (is this last part necessary?)
+
+        CRM_Utils_System::redirect('/civicrm/activity?atype='.$this->get('activity_type_id').'&action=view&reset=1&id='.$this->get('activity_id').'&cid='.$this->get('contact_id').'&context=activity&searchContext=activity');
     }
-}
-
-class onScreenLogger{
-
-  function __construct(){
-    $this->error = 0;
-  }
-
-  function abort($message, $record){
-    $this->error = 1;
-    $this->messages[]=$message;
-  }
-
-  function logDebug($message, $record){
-    $this->messages[]=$message;
-  }
-
-  function logError($message, $record){
-    $this->error = 1;
-    $this->messages[]=$message;
-  }
 }
