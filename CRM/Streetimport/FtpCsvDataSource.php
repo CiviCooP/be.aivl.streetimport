e20010f0fcb5d286081e83cd8660e1099ac2e330
B. Endres <endres@systopia.de>
2024-02-23 20:34:42 +0100

    declare placeholder abstract

--- CRM/Streetimport/FtpCsvDataSource.php
+++ CRM/Streetimport/FtpCsvDataSource.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * This importer will pull CSV files from an FTP server
- * It will then make use of the CRM_Streetimport_FileCsvDataSource to 
+ * It will then make use of the CRM_Streetimport_FileCsvDataSource to
  * read those files.
+ *
+ * THIS IS A PLACEHOLDER, IT'S NOT IMPLEMENTED YET
  *
  * @author Björn Endres (SYSTOPIA) <endres@systopia.de>
  * @license AGPL-3.0
  */
-class CRM_Streetimport_FtpCsvDataSource extends CRM_Streetimport_DataSource {
+abstract class CRM_Streetimport_FtpCsvDataSource extends CRM_Streetimport_DataSource {
   // TODO: implement
 }
